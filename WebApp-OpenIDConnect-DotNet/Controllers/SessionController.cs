86517b8044343c0c365e539c06b6aab238d985a0
Christer Ljung <cljung@live.se>
2019-12-18 16:35:52 +0100

    addded sources

---
+++ WebApp-OpenIDConnect-DotNet/Controllers/SessionController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Http.Features.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using System.IO;
+using System.Net;
+
+namespace WebApp_OpenIDConnect_DotNet.Controllers
+{
+    public class SessionController : Controller
+    {
+        public SessionController(IOptions<AzureAdB2COptions> b2cOptions)
+        {
+            AzureAdB2COptions = b2cOptions.Value;
+        }
+
+        public AzureAdB2COptions AzureAdB2COptions { get; set; }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            var redirectUrl = Url.Action(nameof(HomeController.Index), "Home");
+            return Challenge(
+                new AuthenticationProperties { RedirectUri = redirectUrl },
+                OpenIdConnectDefaults.AuthenticationScheme);
+        }
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            // if we are being called with the email parameter, then we invoke the B2C sign-up policy with the email querystring
+            // so we preset that value
+            string email = Request.Query["email"];
+            var redirectUrl = Url.Action(nameof(HomeController.Index), "Home");
+            var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+            properties.Items[AzureAdB2COptions.PolicyAuthenticationProperty] = AzureAdB2COptions.SignUpPolicyId;
+            if (!string.IsNullOrEmpty(email))
+            {
+                properties.Items.Add("email", email);
+            }
+            return Challenge(properties, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet]
+        public IActionResult ResetPassword()
+        {
+            var redirectUrl = Url.Action(nameof(HomeController.Index), "Home");
+            var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+
+            // reset password when not authenticated means we have pressed the "forget my password" link
+            //
+            // B2C really return a http body with 
+            //  error: access_denied
+            //  error_description: AADB2C90118: The user has forgotten their password
+            // but that gets eaten in the /signin-oidc handler, which passes it on to here. But since we only can do
+            // ResetPassword if we are logged in (link disable otherwise) it must mean that qwe have forgotten our password
+
+            if ( !User.Identity.IsAuthenticated )
+            {
+                properties.Items[AzureAdB2COptions.PolicyAuthenticationProperty] = AzureAdB2COptions.ForgotPasswordPolicyId;
+            }
+            else
+            {
+                properties.Items[AzureAdB2COptions.PolicyAuthenticationProperty] = AzureAdB2COptions.ResetPasswordPolicyId;
+            }
+            return Challenge(properties, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet]
+        public IActionResult EditProfile()
+        {
+            var redirectUrl = Url.Action(nameof(HomeController.Index), "Home");
+            var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+            properties.Items[AzureAdB2COptions.PolicyAuthenticationProperty] = AzureAdB2COptions.EditProfilePolicyId;
+            return Challenge(properties, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet]
+        public IActionResult SignOut()
+        {
+            var callbackUrl = Url.Action(nameof(SignedOut), "Session", values: null, protocol: Request.Scheme);
+            return SignOut(new AuthenticationProperties { RedirectUri = callbackUrl },
+                CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet]
+        public IActionResult SignedOut()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                // Redirect to home page if the user is authenticated.
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ WebApp-OpenIDConnect-DotNet/Startup.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Logging;
+using System;
+
+namespace WebApp_OpenIDConnect_DotNet
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true) // set optional:true to make below override settings
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+        
+            string activeSectionName = Configuration.GetValue<string>("Authentication:AzureAdB2C-Config");
+            string sectionName = "Authentication:" + activeSectionName;
+            services.AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddAzureAdB2C(options => Configuration.Bind( sectionName, options))
+            .AddCookie();
+
+            // Add framework services.
+            services.AddMvc();
+            
+            // Adds a default in-memory implementation of IDistributedCache.
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromHours(1);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            // for dotnet core 2.2. Doesn't work in 3.0
+            loggerFactory.AddConsole();
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                IdentityModelEventSource.ShowPII = true;
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            
+            app.UseSession();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    } // cls
+} // ns
