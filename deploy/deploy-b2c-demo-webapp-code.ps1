8d0d831147b299da05529c6c47606f47b255922e
Christer Ljung <cljung@live.se>
2019-12-23 22:02:46 +0100

    publish code

---
+++ deploy/deploy-b2c-demo-webapp-code.ps1
@@ -0,0 +1,6 @@
+param (
+    [Parameter(Mandatory=$True)][Alias('r')][string]$ResourceGroupName = "",
+    [Parameter(Mandatory=$True)][Alias('n')][string]$WebAppName = ""
+)
+
+Publish-AzWebapp -ResourceGroupName $ResourceGroupName -Name $WebAppName -ArchivePath .\WebApp.zip
