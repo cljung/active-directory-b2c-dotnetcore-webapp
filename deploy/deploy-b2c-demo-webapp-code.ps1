6e883c2037022adddd4c09ace0c460301088382d
Christer Ljung <cljung@live.se>
2019-12-26 10:14:49 +0100

    force

--- deploy/deploy-b2c-demo-webapp-code.ps1
+++ deploy/deploy-b2c-demo-webapp-code.ps1
@@ -3,4 +3,4 @@ param (
     [Parameter(Mandatory=$True)][Alias('n')][string]$WebAppName = ""
 )
 
-Publish-AzWebapp -ResourceGroupName $ResourceGroupName -Name $WebAppName -ArchivePath .\WebApp.zip
+Publish-AzWebapp -ResourceGroupName $ResourceGroupName -Name $WebAppName -ArchivePath .\WebApp.zip -Force
